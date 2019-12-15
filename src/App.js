8ac87f70fba4cd916749dc1ee0f69df1592db0fb
Lincoln Ruteski <lruteski@gmail.com>
2019-12-15 08:11:39 -0300

    hoock useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   const [techs, setTech] = useState([
+      'ReactJS',
+      'ReactNative'
+   ]);
+
+   const [newTech, setNewTech] = useState();
+
+   function handleAdd() {
+      setTech([...techs, newTech]);
+      setNewTech('');
+   }
+
+   return (
+      <>
+         <ul>
+            {techs.map(t => (
+               <li key={t}>{t}</li>
+            ))}
+         </ul>
+         <input value={newTech} onChange={e => setNewTech(e.target.value)}/>
+         <button type="button" onClick={handleAdd}>Adicionar</button>
+      </>
   );
 }
 
